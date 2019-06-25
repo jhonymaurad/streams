39fd90d862f27ce2140ad8c5c4d082786b5baba3
jhony maurad <jhonymaurad@gmail.com>
2019-06-24 21:54:19 -0400

    added streams basic components and routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+
+
 
 const App = () =>{
     return(
-        <div>App</div>
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route exact path='/' component={StreamList}/>
+                    <Route exact path='/streams/new' component={StreamCreate}/>
+                    <Route exact path='/streams/edit' component={StreamEdit}/>
+                    <Route exact path='/streams/delete' component={StreamDelete}/>
+                    <Route exact path='/streams/show' component={StreamShow}/>
+
+
+                </div>
+
+            </BrowserRouter>
+        </div>
     )
 }
 
