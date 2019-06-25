65bb7d8697c750d41162f92a06ba6aee3f732aae
jhony maurad <jhonymaurad@gmail.com>
2019-06-24 22:12:37 -0400

    Add Header component and semantic ui min css

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
@@ -10,9 +11,11 @@ import StreamShow from './streams/StreamShow';
 
 const App = () =>{
     return(
-        <div>
+        <div className='ui container'>
+            
             <BrowserRouter>
                 <div>
+                    <Header />
                     <Route exact path='/' component={StreamList}/>
                     <Route exact path='/streams/new' component={StreamCreate}/>
                     <Route exact path='/streams/edit' component={StreamEdit}/>
