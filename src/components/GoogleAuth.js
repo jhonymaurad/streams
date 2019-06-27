1c6176fd50c8be63e63101d841eb06d64c4bd02d
jhony maurad <jhonymaurad@gmail.com>
2019-06-27 00:03:53 -0400

    Add redux dev tools extension and refactor GoogleAuth component

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -32,7 +32,7 @@ class GoogleAuth extends React.Component{
     // REDUX:
     onAuthChange = (isSignedIn) => {
         if(isSignedIn){
-            this.props.signIn();
+            this.props.signIn(this.auth.currentUser.get().getId());
         }else{
             this.props.signOut();
         }
