3a40d4f5043b7b9c09883dc17f20db3d63ef60e0
jhony maurad <jhonymaurad@gmail.com>
2019-06-28 00:24:20 -0400

    Customized form fiels and destructured formProps(which includes: input)

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -3,18 +3,23 @@ import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends Component{
 
-    renderInput = (formProps) => {
+    renderInput = ( {input, label}) => { //need to destructure label
         //console.log(formProps)
         // return <input onChange={formProps.input.onChange} value={formProps.input.value} />
-        return <input {...formProps.input}/>
+        return (
+            <div className='field'>
+                <label>{label}</label>
+                <input {...input}/>
+            </div>
+        )
     }
 
     render(){
         //console.log(this.props)
         return (
-            <form>
-                <Field name='title' component={this.renderInput} />
-                <Field name='description' component={this.renderInput} />
+            <form className='ui form'>
+                <Field name='title' component={this.renderInput} label='Enter Title: ' />
+                <Field name='description' component={this.renderInput} label='Enter Description: ' />
             </form>
         );
     }; 
