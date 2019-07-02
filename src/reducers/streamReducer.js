50423e10ba060931199f188a7744a0f6b137e52d
jhony maurad <jhonymaurad@gmail.com>
2019-07-01 22:22:18 -0400

    fetched all streams from db

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -16,7 +16,7 @@ export default (state= {}, action)=>{
         case EDIT_STREAM:
             return {...state, [action.payload.id]: action.payload};
         case FETCH_STREAMS:
-            return ;
+            return {...state, ..._.mapKeys(action.payload, 'id')} ;
         case DELETE_STREAM:
             return _.omit(state, action.payload) ;
         default:
