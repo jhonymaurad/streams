95dedbb7fbbd081f9591c2bdcba0ce4890587ba3
jhony maurad <jhonymaurad@gmail.com>
2019-07-21 18:55:59 -0400

    Finish MVP of the project

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -20,6 +20,9 @@ class StreamShow extends React.Component {
         this.buildPlayer();
     }
 
+    componentWillUnmount(){
+        this.player.destroy();
+    }
     buildPlayer() {
         if(this.player || !this.props.stream){
             return;
